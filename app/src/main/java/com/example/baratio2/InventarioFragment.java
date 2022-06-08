e62c96b98aca0318ca5572d273dc6949eec0d0e9
Feivian4 <feivian21@gmail.com>
2022-06-08 06:48:59 -0500

    CrearProductoFragment

--- app/src/main/java/com/example/baratio2/InventarioFragment.java
+++ app/src/main/java/com/example/baratio2/InventarioFragment.java
@@ -1,5 +1,8 @@
 package com.example.baratio2;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 
@@ -16,6 +23,7 @@ public class InventarioFragment extends Fragment {
 
     private InventarioListAdapter inventarioListAdapter;
     private RecyclerView lstProductos;
+    private SharedPreferences sharedPreferences;
     View root;
 
     public InventarioFragment() {
@@ -34,6 +42,7 @@ public class InventarioFragment extends Fragment {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+        sharedPreferences = this.getActivity().getSharedPreferences("MySharedPref", MODE_PRIVATE);
     }
 
     @Override
@@ -48,14 +57,34 @@ public class InventarioFragment extends Fragment {
     public void init() {
         inventarioListAdapter = new InventarioListAdapter();
         ArrayList<Producto> productoList = new ArrayList<>();
-        productoList.add(new Producto("vd52", "Verdura", "5", "La Granja",
-                "Verdura Fresca", 120));
-        productoList.add(new Producto("pp33", "Pechuga de Pollo", "55",
-                "La Granja", "Pechuga de Pollo", 244));
+        Gson gson = new Gson();
+        if(!sharedPreferences.contains("productoList")){
+            productoList.add(new Producto("vd52", "Verdura", "5", "La Granja",
+                    "Verdura Fresca", 120));
+            productoList.add(new Producto("pp33", "Pechuga de Pollo", "55",
+                    "La Granja", "Pechuga de Pollo", 244));
+            SharedPreferences.Editor prefsEditor = sharedPreferences.edit();
+            String json = gson.toJson(productoList);
+            prefsEditor.putString("productoList", json);
+            prefsEditor.commit();
+        }
+        else{
+            String json = sharedPreferences.getString("productoList", "");
+            productoList = gson.fromJson(json, new TypeToken<ArrayList<Producto>>() {
+            }.getType());
+        }
         inventarioListAdapter.submitList(productoList);
         lstProductos = root.findViewById(R.id.lstProductos);
         lstProductos.setLayoutManager(new LinearLayoutManager(root.getContext()));
         lstProductos.setAdapter(inventarioListAdapter);
         inventarioListAdapter.notifyDataSetChanged();
+        ImageButton button= (ImageButton) root.findViewById(R.id.btnCrear);
+        button.setOnClickListener(view -> {
+            CrearProductoFragment nextFrag= new CrearProductoFragment();
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_parent, nextFrag, "findThisFragment")
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 }
