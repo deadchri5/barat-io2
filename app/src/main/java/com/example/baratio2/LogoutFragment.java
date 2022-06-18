4efb923fe8cd60837d829a113c01f58f4b2fa1ab
PJ <pepehernandezdelabarranca@gmail.com>
2022-06-17 22:12:57 -0500

    Funcionalidad de Logout en un fragmento ademas de actualizaciones del diseño de navbar

---
+++ app/src/main/java/com/example/baratio2/LogoutFragment.java
@@ -0,0 +1,45 @@
+package com.example.baratio2;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LogoutFragment extends Fragment {
+    View root;
+    public LogoutFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static LogoutFragment newInstance() {
+        LogoutFragment fragment = new LogoutFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Intent intent = new Intent(LogoutFragment.this.getActivity(), LoginActivity.class);
+        startActivity(intent);
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        root = inflater.inflate(R.layout.fragment_logout, container, false);
+        init();
+        return root;
+    }
+    public void init(){
+    }
+}
