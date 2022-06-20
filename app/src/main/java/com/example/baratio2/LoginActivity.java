94531b99943e5260a9af5cd5af3eea6e47ec1c5f
Christian <a20110469@ceti.mx>
2022-06-20 00:25:48 -0500

    login completo
    login

--- app/src/main/java/com/example/baratio2/LoginActivity.java
+++ app/src/main/java/com/example/baratio2/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.baratio2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,22 +10,36 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+
 public class LoginActivity extends AppCompatActivity {
 
     Button salir, iniciar;
+    SharedPreferences sharedPreferences;
+    ArrayList<Usuario> usuarioList;
     EditText user, pass;
+    boolean acceso = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        Gson gson = new Gson();
+        sharedPreferences = this.getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        if (sharedPreferences.contains("userList")) {
+            String json = sharedPreferences.getString("userList", "");
+            usuarioList = gson.fromJson(json, new TypeToken<ArrayList<Usuario>>() {
+            }.getType());
+        }
         salir = findViewById(R.id.salir);
         iniciar = findViewById(R.id.iniciar);
         user = findViewById(R.id.user);
         pass = findViewById(R.id.pass);
-        user.setText("fabian");
-        pass.setText("fabian123");
+        user.setText("root");
+        pass.setText("admin");
 
 salir.setOnClickListener(new View.OnClickListener() {
     @Override
@@ -38,11 +53,24 @@ salir.setOnClickListener(new View.OnClickListener() {
 iniciar.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
-        if(user.getText().toString().equals("fabian") && pass.getText().toString().equals("fabian123")){
+        String usr = user.getText().toString();
+        String psw = pass.getText().toString();
+        for (int i = 0; i < usuarioList.size(); i++) {
+            if (usuarioList.get(i).getNombre().equals(usr) && usuarioList.get(i).getPass().equals(psw)) {
+                Toast.makeText(LoginActivity.this, "Bienvenido " +
+                        usuarioList.get(i).getNombre(), Toast.LENGTH_SHORT).show();
+                acceso = true;
+            } else if (user.getText().toString().equals("root") && pass.getText().toString().equals("admin")) {
+                Toast.makeText(LoginActivity.this, "Bienvenido ROOT", Toast.LENGTH_SHORT).show();
+                acceso = true;
+            }
+        }
+        if (acceso) {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
-        }else {
-            Toast.makeText(LoginActivity.this, "Verifique sus credenciales", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this, "Verifique sus credenciales",
+                    Toast.LENGTH_SHORT).show();
         }
     }
 });
